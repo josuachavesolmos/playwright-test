5458a23f29a5c08084b543d92c71288da31606d6
Josua Chaves <josua.chaves@axessgroup.com>
2025-04-23 16:24:35 -0600

    primer test con playwright

---
+++ tests/example.spec.js
@@ -0,0 +1,6 @@
+﻿const { test, expect } = require('@playwright/test');
+
+test('verifica el título de Playwright', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
